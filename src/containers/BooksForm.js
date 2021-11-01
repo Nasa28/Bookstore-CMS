d69055b9eb10650de056a8c936b79d0430246597
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-01 15:02:42 +0100

    Implement BookForm component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Select from 'react-select';
 
 const BooksForm = () => {
+  const options = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
   return (
     <div>
-      
+      <form>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <label>
+          Category:
+          <Select options={options} name="category" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default BooksForm
+export default BooksForm;
