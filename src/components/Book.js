bdec81ff0a38fbe5a82472dd9ad9acfd810413a6
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-08 21:12:12 +0100

    Modify css

--- src/components/Book.js
+++ src/components/Book.js
@@ -13,28 +13,42 @@ const Book = (props) => {
           <h3 className="title">{title}</h3>
           <span className="author">Author</span>
           <div className="btns">
-            <button className="btn" type="button">
-              Comments
-            </button>
-            <button className="btn" onClick={handleRemoveBook} type="button">
-              Remove
-            </button>
-            <button className="btn" type="button">
-              Edit
-            </button>
+            <div className="divides">
+              <button className="btn d-block" type="button">
+                Comments
+              </button>
+
+              <div className="link-divider" />
+            </div>
+            <div className="divides">
+              <button
+                className="btn d-block"
+                onClick={handleRemoveBook}
+                type="button"
+              >
+                Remove
+              </button>
+
+              <div className="link-divider" />
+            </div>
+            <div>
+              <button className="btn" type="button">
+                Edit
+              </button>
+            </div>
           </div>
         </div>
-
-        <div className="circle">
-          <div className="round" />
-          <div>
-            {`${num}%`}
-            <p className="completed">Completed</p>
+        <div className="divide">
+          <div className="circle">
+            <div className="round" />
+            <div>
+              {`${num}%`}
+              <p className="completed">Completed</p>
+            </div>
           </div>
-        </div>
-
-        <div className="update-progress">
           <div className="divider" />
+        </div>
+        <div className="update-progress">
           <p className="current">CURRENT CHAPTER</p>
           <p className="chapter">Chapter No</p>
           <button type="button" className="update-btn">
